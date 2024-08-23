0a9128516d79ade4302ca4965b246c6277f301c8
staocube88 <staocube@gmail.com>
2024-08-23 12:29:59 +0100

    route to root

--- node-app/index.js
+++ node-app/index.js
@@ -3,6 +3,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello from Node.js API!' });
+});
+
 app.get('/api/greeting', (req, res) => {
   res.json({ message: 'Hello from Node.js API!' });
 });
